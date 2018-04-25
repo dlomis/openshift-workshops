96a756d160402710a43beba77ebd6a47ed11ad23
Darren Lomis <darmis1@gmail.com>
2018-04-25 14:02:10 -0400

    Update index.js

--- dc-metro-map/routes/index.js
+++ dc-metro-map/routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var rest = require('restler');
 
-alert("TEST!");
 //-----------------------------------------------------------------------------
 // CONSTANTS AND HELPERS
 //-----------------------------------------------------------------------------
